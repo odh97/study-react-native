e1251fb1892d77774c5e025e2e391da9f7f2b378
od <xzdheogus1@vanillacode.com>
2024-03-05 20:39:24 +0900

    test

--- App.tsx
+++ App.tsx
@@ -72,6 +72,7 @@ function App(): React.JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
+        <Text>123</Text>
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
